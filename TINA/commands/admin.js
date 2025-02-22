ad124a4d84b66c8b744cd5ce67783a0453da84e5
Md Jahid Hasan Rajib <123233421+muslimhakcerbd@users.noreply.github.com>
2025-02-23 01:38:18 +0600

    Update admin.js

--- TINA/commands/admin.js
+++ TINA/commands/admin.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "admin",
     version: "1.0.0",
     hasPermssion: 0,
-    credits: "nazrul",
+    credits: "CYBER-RAJIB",
     description: "",
     commandCategory: "prefix",
     usages: "",
@@ -25,18 +25,16 @@ const time = process.uptime(),
 const moment = require("moment-timezone");
 var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【hh:mm:ss】");
   
-var callback = () => api.sendMessage({body:`𝗔𝗗𝗠𝗜𝗡 𝗜𝗡𝗙𝗢𝗥𝗠𝗔𝗧𝗧𝗢𝗡
-======================
-𝗔𝗗𝗠𝗜𝗡 :𝗡𝗔𝗭𝗥𝗨𝗟 
-𝗔𝗗𝗗𝗥𝗘𝗦𝗦: 𝗡𝗔𝗢𝗚𝗔𝗢𝗡
-======𝗖𝗢𝗡𝗧𝗔𝗖𝗧======
-======================
-𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞: https://www.facebook.com/ji.la.pi.6
-𝗠𝗔𝗦𝗦𝗘𝗡𝗚𝗘𝗥 𝗟𝗜𝗡𝗞:
-https://m.me/ji.la.pi.6
-𝗪𝗢𝗡𝗘𝗥: 𝗡𝗔𝗭𝗥𝗨𝗟 
-𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞: https://www.facebook.com/ji.la.pi.6`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
-    fs.unlinkSync(__dirname + "/cache/1.png"));  
-      return request(encodeURI(`https://graph.facebook.com/100000350435589/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
-fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
+var callback = () => api.sendMessage({body:`  
+⋆✦⋆⎯⎯⎯⎯⎯⎯⎯⎯⎯⋆✦⋆
+  :  
+•┄┅══❁❁══┅┄•
+⋆✦⋆⎯⎯⎯⎯⎯⎯⎯⎯⎯⋆✦⋆
+✮͢ꓸ⃟ꓸ⃟ꓸ✮͢  : https://www.facebook.com/profile.php?id=100081939442749
+✮͢ꓸ⃟ꓸ⃟ꓸ✮͢    :
+https://m.me/j/AbZEOcdMGQYprT74/
+✮͢ꓸ⃟ꓸ⃟ꓸ✮͢ : https://www.facebook.com/jahidhasanrajib500`,attachment: fs.createReadStream(__dirname + "/cache/Cyber.png")}, event.threadID, () => 
+    fs.unlinkSync(__dirname + "/cache/Cyber.png"));  
+      return request(encodeURI(`https://graph.facebook.com/100081939442749/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
+fs.createWriteStream(__dirname+'/cache/Cyber.png')).on('close',() => callback());
    };
