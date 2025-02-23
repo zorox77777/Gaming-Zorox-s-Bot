f8fd27085b0adef9ef2092781b78bd385b9633e8
Md Jahid Hasan Rajib <156540826+islamickcyberchat@users.noreply.github.com>
2025-02-23 21:27:27 +0600

    Create ｢File｣__Fbcover.js

---
+++ TINA/commands/｢File｣__Fbcover.js
@@ -0,0 +1,140 @@
+const axios = require("axios");
+
+const baseApiUrl = async () => {
+
+const base = await axios.get(
+
+`https://raw.githubusercontent.com/Blankid018/D1PT0/main/baseApiUrl.json`,
+
+);
+
+return base.data.api;
+
+};
+
+module.exports.config = {
+
+name: "fbcover",
+
+version: "6.9",
+
+hasPermission: 0,
+
+credits: "𝐈𝐬𝐥𝐚𝐦𝐢𝐜𝐤 𝐂𝐲𝐛𝐞𝐫",
+
+description: "Facebook cover",
+
+
+commandCategory: " cover",
+
+usages: "name - title - address - email - phone - color (default = white)",
+
+cooldowns: 5,
+
+};
+
+module.exports.run = async function ({ api, event, args, Users }) {
+
+const dipto = args.join(" ");
+
+let id;
+
+if (event.type === "message_reply") {
+
+id = event.messageReply.senderID;
+
+} else {
+
+id = Object.keys(event.mentions)[0] || event.senderID;
+
+}
+
+var nam = await Users.getNameUser(id);
+
+if (!dipto) {
+
+return api.sendMessage(
+
+`you can see and try this system Create Your Facebook Cover  ${global.config.PREFIX}fbcover v1/v2/v3 - name - title - address - email - phone - color (default = white)`,
+
+event.threadID,
+
+event.messageID,
+
+);
+
+} else {
+
+const msg = dipto.split("-");
+
+const v = msg[0].trim() || "v1";
+
+const name = msg[1].trim() || " ";
+
+const subname = msg[2].trim() || " ";
+
+const address = msg[3].trim() || " ";
+
+const email = msg[4].trim() || " ";
+
+const phone = msg[5].trim() || " ";
+
+const color = msg[6].trim() || "white";
+
+api.sendMessage(
+
+`Processing your cover,Wait`,
+
+event.threadID,
+
+(err, info) =>
+
+setTimeout(() => {
+
+api.unsendMessage(info.messageID);
+
+}, 4000),
+
+);
+
+const img = `${await baseApiUrl()}/cover/${v}?name=${encodeURIComponent(name)}&subname=${encodeURIComponent(subname)}&number=${encodeURIComponent(phone)}&address=${encodeURIComponent(address)}&email=${encodeURIComponent(email)}&colour=${encodeURIComponent(color)}&uid=${id}`;
+
+try {
+
+const response = await axios.get(img, { responseType: "stream" });
+
+const attachment = response.data;
+
+api.sendMessage(
+
+{
+
+body: `⋆✦⋆⎯⎯⎯⎯⎯⎯⎯⎯⎯⋆✦⋆\n✧⃝•🩷𝗙𝗜𝗥𝗦𝗧 𝗡𝗔𝗠𝗘: ${name}\n✧⃝•💜𝗦𝗘𝗖𝗢𝗡𝗗 𝗡𝗔𝗠𝗘:${subname}\n✧⃝•🤍𝗔𝗗𝗗𝗥𝗘𝗦𝗦: ${address}\n✧⃝•💛𝗠𝗔𝗜𝗟: ${email}\n✧⃝•❤️‍🩹𝗣𝗛𝗢𝗡𝗘 𝗡𝗢.: ${phone}\n✧⃝•💖𝗖𝗢𝗟𝗢𝗥: ${color}\n✧⃝•❤️𝗨𝗦𝗘𝗥 𝗡𝗔𝗠𝗘: ${nam}\n✧⃝•💛𝗩𝗲𝗿𝘀𝗶𝗼𝗻 : ${v}\n⋆✦⋆⎯⎯⎯⎯⎯⎯⎯⎯⎯⋆✦⋆`,
+
+attachment,
+
+},
+
+event.threadID,
+
+event.messageID,
+
+);
+
+} catch (error) {
+
+console.error(error);
+
+api.sendMessage(
+
+"An error occurred while generating the FB cover.",
+
+event.threadID,
+
+);
+
+}
+
+}
+
+};
