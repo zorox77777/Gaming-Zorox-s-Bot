6b18277f8a9596faa0aad58a6e4157c0e1216c26
Md Jahid Hasan Rajib <123233421+muslimhakcerbd@users.noreply.github.com>
2025-02-24 15:44:15 +0600

    Update log.js

--- utils/log.js
+++ utils/log.js
@@ -1,36 +1,36 @@
-const chalk = require('chalk');
+const chalk = require("chalk");
 function randomColor() {
-    var color = "";
-    for (var i = 0; i < 3; i++) {
-        var sub = Math.floor(Math.random() * 256).toString(16);
-        color += (sub.length == 1 ? "0" + sub : sub);
-    } 
-   return "#" + color;
+  var _0x12f4e2 = '';
+  for (var _0x50695d = 0; _0x50695d < 3; _0x50695d++) {
+    var _0x4a1ebb = Math.floor(Math.random() * 256).toString(16);
+    _0x12f4e2 += _0x4a1ebb.length == 1 ? '0' + _0x4a1ebb : _0x4a1ebb;
+  }
+  return '#' + _0x12f4e2;
+}
+;
+module.exports = (_0x44107f, _0x509531) => {
+  switch (_0x509531) {
+    case "warn":
+      console.log(chalk.bold.hex("#ff0000").bold("» Log « ") + _0x44107f);
+      break;
+    case "error":
+      console.log(chalk.bold.hex("#ff0000").bold("» Log « ") + _0x44107f);
+      break;
+    default:
+      console.log(chalk.bold.hex(randomColor()).bold(_0x509531 + " » ") + _0x44107f);
+      break;
+  }
 };
-module.exports = (data, option) => {
-  switch (option) {
+module.exports.loader = (_0x4705f2, _0x10a189) => {
+  switch (_0x10a189) {
     case "warn":
-        console.log(chalk.bold.hex("#ff0000").bold('» Log « ') + data);
+      console.log(chalk.bold.hex(randomColor()).bold(" CYBER ERROR•—»✨ ") + chalk.bold.hex("#8B8878").bold(_0x4705f2) + chalk.bold.hex("FF00DD")(''));
       break;
     case "error":
-      console.log(chalk.bold.hex("#ff0000").bold('» Log « ') + data);
+      console.log(chalk.bold.hex(randomColor()).bold(" CYBER ERROR •—»✨ ") + _0x4705f2 + chalk.bold.hex("5EFF00")(''));
       break;
     default:
-        console.log(chalk.bold.hex(randomColor()).bold(`${option} » `) + data);
+      console.log(chalk.bold.hex(randomColor()).bold("CYBER•—»✨ ") + chalk.bold.hex(randomColor()).bold(_0x4705f2) + chalk.bold.hex("FFF0000")(''));
       break;
   }
-}
-
-module.exports.loader = (data, option) => {
-  switch (option) {
-    case "warn":
-      console.log(chalk.bold.hex(randomColor()).bold(" TINA ERROR➜ ") + chalk.bold.hex("#8B8878").bold(data)+chalk.bold.hex("FF00DD")("" ))
-      break;
-    case "error":
-    console.log(chalk.bold.hex(randomColor()).bold(" NAZRUL ERROR➜ ") + data+chalk.bold.hex("5EFF00")("" ));
-      break;
-    default:
-      console.log(chalk.bold.hex(randomColor()).bold(" NAZRUL➜ ") + chalk.bold.hex(randomColor()).bold(data) +chalk.bold.hex("FFF0000")("" ));
-      break;
-  }
-}
+};
