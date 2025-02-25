f0bca94709f88a95e5132b6fae8595c034ad2803
Md Jahid Hasan Rajib <123233421+muslimhakcerbd@users.noreply.github.com>
2025-02-25 19:31:16 +0600

    Update ｢File｣__Groupimg.js

--- Script/commands/｢File｣__Groupimg.js
+++ Script/commands/｢File｣__Groupimg.js
@@ -4,7 +4,7 @@ module.exports.config = {
 	name: "groupimage",
 	version: "1.0.0", 
 	hasPermssion: 0,
-	credits: "",
+	credits: "𝐈𝐬𝐥𝐚𝐦𝐢𝐜𝐤 𝐂𝐲𝐛𝐞𝐫",
 	description: "Change your group image",
 	commandCategory: "Box", 
 	usages: "groupimage", 
@@ -13,12 +13,12 @@ module.exports.config = {
 };
 
 module.exports.run = async ({ api, event }) => {
-	if (event.type !== "message_reply") return api.sendMessage("You have to reply to a photo...!️", event.threadID, event.messageID);
-	if (!event.messageReply.attachments || event.messageReply.attachments.length == 0) return api.sendMessage("You have to reply to a photo...!️", event.threadID, event.messageID);
+	if (event.type !== "message_reply") return api.sendMessage("❌ You have to reply to a photo", event.threadID, event.messageID);
+	if (!event.messageReply.attachments || event.messageReply.attachments.length == 0) return api.sendMessage("❌ You have to reply to a photo", event.threadID, event.messageID);
 	if (event.messageReply.attachments.length > 1) return api.sendMessage(`Please reply only 1 photo!`, event.threadID, event.messageID);
 	var abc = event.messageReply.attachments[0].url
-	let pathImg = __dirname + '/cyber/loz.png';
+	let pathImg = __dirname + '/cache/loz.png';
     let getdata = (await axios.get(`${abc}`, { responseType: 'arraybuffer' })).data;
   fs.writeFileSync(pathImg, Buffer.from(getdata, 'utf-8'));
-    return api.changeGroupImage(fs.createReadStream(__dirname + '/cyber/loz.png'), event.threadID, () => fs.unlinkSync(pathImg), event.messageID);
+    return api.changeGroupImage(fs.createReadStream(__dirname + '/cache/loz.png'), event.threadID, () => fs.unlinkSync(pathImg), event.messageID);
   }
