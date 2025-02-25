7455bfa259ce909e8473fbdbccecdf8d8685d7c4
islamick <mdnurnoby5767@gmail.com>
2025-02-25 18:50:09 +0600

    Update listen.js

--- includes/listen.js
+++ includes/listen.js
@@ -7,12 +7,12 @@ module.exports = function({ api, models }) {
 	const fs = require("fs");
 	const moment = require('moment-timezone');
 	const axios = require("axios");
-  var day = moment.tz("Asia/Dhaka").day();
+  var day = moment.tz("Asia/Kolkata").day();
   
   
-  const checkttDataPath = __dirname + '/../TINA/commands/checktt/';
+  const checkttDataPath = __dirname + '/../Script/commands/checktt/';
   setInterval(async() => {
-    const day_now = moment.tz("Asia/Dhaka").day();
+    const day_now = moment.tz("Asia/Kolkata").day();
     if (day != day_now) {
       day = day_now;
       const checkttData = fs.readdirSync(checkttDataPath);
@@ -117,4 +117,171 @@ module.exports = function({ api, models }) {
         for (const dataU of users) {
             const idUsers = String(dataU['userID']);
             global.data['allUserID']['push'](idUsers);
-            if (dataU.name 
+            if (dataU.name && dataU.name['length'] != 0) global.data.userName['set'](idUsers, dataU.name);
+            if (dataU.data && dataU.data.banned == 1) global.data['userBanned']['set'](idUsers, {
+                'reason': dataU['data']['reason'] || '',
+                'dateAdded': dataU['data']['dateAdded'] || ''
+            });
+            if (dataU['data'] && dataU.data['commandBanned'] && dataU['data']['commandBanned']['length'] != 0) 
+            global['data']['commandBanned']['set'](idUsers, dataU['data']['commandBanned']);
+        }
+        for (const dataC of currencies) global.data.allCurrenciesID.push(String(dataC['userID']));
+        logger.loader(global.getText('listen', 'loadedEnvironmentUser')), logger(global.getText('listen','successLoadEnvironment'),'[ DATABASE ]');
+    } catch (error) {
+        return logger.loader(global.getText('listen', 'failLoadEnvironment', error), 'error');
+    }
+}());
+	logger(`${api.getCurrentUserID()} - [ ${global.config.PREFIX} ] • ${(!global.config.BOTNAME) ? "THIS BOT WAS MADE BY NAZRUL" : global.config.BOTNAME}`, "[ BOT INFO ]");
+	
+	///////////////////////////////////////////////
+	//========= Require all handle need =========//
+	//////////////////////////////////////////////
+
+	const handleCommand = require("./handle/handleCommand")({ api, models, Users, Threads, Currencies });
+	const handleCommandEvent = require("./handle/handleCommandEvent")({ api, models, Users, Threads, Currencies });
+	const handleReply = require("./handle/handleReply")({ api, models, Users, Threads, Currencies });
+	const handleReaction = require("./handle/handleReaction")({ api, models, Users, Threads, Currencies });
+	const handleEvent = require("./handle/handleEvent")({ api, models, Users, Threads, Currencies });
+	const handleCreateDatabase = require("./handle/handleCreateDatabase")({  api, Threads, Users, Currencies, models });
+
+	logger.loader(`====== ${Date.now() - global.client.timeStart}ms ======`);
+
+
+	//DEFINE DATLICH PATH
+	const datlichPath = __dirname + '/../Script/commands/cache/datlich.json';
+
+	//FUNCTION HOẠT ĐỘNG NHƯ CÁI TÊN CỦA NÓ, CRE: DUNGUWU
+	const monthToMSObj = {
+		1: 31 * 24 * 60 * 60 * 1000,
+		2: 28 * 24 * 60 * 60 * 1000,
+		3: 31 * 24 * 60 * 60 * 1000,
+		4: 30 * 24 * 60 * 60 * 1000,
+		5: 31 * 24 * 60 * 60 * 1000,
+		6: 30 * 24 * 60 * 60 * 1000,
+		7: 31 * 24 * 60 * 60 * 1000,
+		8: 31 * 24 * 60 * 60 * 1000,
+		9: 30 * 24 * 60 * 60 * 1000,
+		10: 31 * 24 * 60 * 60 * 1000,
+		11: 30 * 24 * 60 * 60 * 1000,
+		12: 31 * 24 * 60 * 60 * 1000
+	};
+	const checkTime = (time) => new Promise((resolve) => {
+		time.forEach((e, i) => time[i] = parseInt(String(e).trim()));
+		const getDayFromMonth = (month) => (month == 0) ? 0 : (month == 2) ? (time[2] % 4 == 0) ? 29 : 28 : ([1, 3, 5, 7, 8, 10, 12].includes(month)) ? 31 : 30;
+		if (time[1] > 12 || time[1] < 1) resolve("Your month doesn't seem valid");
+		if (time[0] > getDayFromMonth(time[1]) || time[0] < 1) resolve("Your date seems invalid");
+		if (time[2] < 2022) resolve("In what era do you live in?");
+		if (time[3] > 23 || time[3] < 0) resolve("Your hours don't seem valid");
+		if (time[4] > 59 || time[3] < 0) resolve("Your minutes don't seem valid");
+		if (time[5] > 59 || time[3] < 0) resolve("Your seconds don't seem valid");
+		yr = time[2] - 1970;
+		yearToMS = (yr) * 365 * 24 * 60 * 60 * 1000;
+		yearToMS += ((yr - 2) / 4).toFixed(0) * 24 * 60 * 60 * 1000;
+		monthToMS = 0;
+		for (let i = 1; i < time[1]; i++) monthToMS += monthToMSObj[i];
+		if (time[2] % 4 == 0) monthToMS += 24 * 60 * 60 * 1000;
+		dayToMS = time[0] * 24 * 60 * 60 * 1000;
+		hourToMS = time[3] * 60 * 60 * 1000;
+		minuteToMS = time[4] * 60 * 1000;
+		secondToMS = time[5] * 1000;
+		oneDayToMS = 24 * 60 * 60 * 1000;
+		timeMs = yearToMS + monthToMS + dayToMS + hourToMS + minuteToMS + secondToMS - oneDayToMS;
+		resolve(timeMs);
+	});
+
+
+	const tenMinutes = 10 * 60 * 1000;
+
+	logger.loader(`====== ${Date.now() - global.client.timeStart}ms ======`);
+	const checkAndExecuteEvent = async () => {
+
+		/*smol check*/
+		if (!fs.existsSync(datlichPath)) fs.writeFileSync(datlichPath, JSON.stringify({}, null, 4));
+		var data = JSON.parse(fs.readFileSync(datlichPath));
+
+		//GET CURRENT TIME
+		var timeVN = moment().tz('Asia/Dhaka').format('DD/MM/YYYU_HH:mm:ss');
+		timeVN = timeVN.split("_");
+		timeVN = [...timeVN[0].split("/"), ...timeVN[1].split(":")];
+
+		let temp = [];
+		let vnMS = await checkTime(timeVN);
+		const compareTime = e => new Promise(async (resolve) => {
+			let getTimeMS = await checkTime(e.split("_"));
+			if (getTimeMS < vnMS) {
+				if (vnMS - getTimeMS < tenMinutes) {
+					data[boxID][e]["TID"] = boxID;
+					temp.push(data[boxID][e]); delete data[boxID][e];
+				} else delete data[boxID][e];
+				fs.writeFileSync(datlichPath, JSON.stringify(data, null, 4));
+			};
+			resolve();
+		})
+
+		await new Promise(async (resolve) => {
+			for (boxID in data) {
+				for (e of Object.keys(data[boxID])) await compareTime(e);
+			}
+			resolve();
+		})
+		for (el of temp) {
+			try {
+				var all = (await Threads.getInfo(el["TID"])).participantIDs;
+			    all.splice(all.indexOf(api.getCurrentUserID()), 1);
+				var body = el.REASON || "MỌI NGƯỜI ƠI", mentions = [], index = 0;
+				
+			    for (let i = 0; i < all.length; i++) {
+				    if (i == body.length) body += " ‍ ";
+				    mentions.push({
+				  	  tag: body[i],
+				  	  id: all[i],
+				  	  fromIndex: i - 1
+				    });
+			    }
+			} catch (e) { return console.log(e); }
+			var out = {
+				body, mentions
+			}
+			if ("ATTACHMENT" in el) {
+				out.attachment = [];
+				for (a of el.ATTACHMENT) {
+					let getAttachment = (await axios.get(encodeURI(a.url), { responseType: "arraybuffer"})).data;
+					fs.writeFileSync(__dirname + `/../Script/commands/cache/${a.fileName}`, Buffer.from(getAttachment, 'utf-8'));
+					out.attachment.push(fs.createReadStream(__dirname + `/../Script/commands/cache/${a.fileName}`));
+				}
+			}
+			console.log(out);
+			if ("BOX" in el) await api.setTitle(el["BOX"], el["TID"]);
+			api.sendMessage(out, el["TID"], () => ("ATTACHMENT" in el) ? el.ATTACHMENT.forEach(a => fs.unlinkSync(__dirname + `/../Script/commands/cache/${a.fileName}`)) : "");
+		}
+
+	}
+	setInterval(checkAndExecuteEvent, tenMinutes/10);
+	
+
+	//////////////////////////////////////////////////
+	//========= Send event to handle need =========//
+	/////////////////////////////////////////////////
+	
+	return (event) => {
+    switch (event.type) {
+      case "message":
+      case "message_reply":
+      case "message_unsend":
+        handleCreateDatabase({ event });
+        handleCommand({ event });
+        handleReply({ event });
+        handleCommandEvent({ event });
+
+        break;
+      case "event":
+        handleEvent({ event });
+        break;
+      case "message_reaction":
+        handleReaction({ event });
+        break;
+      default:
+        break;
+    }
+  };
+};
