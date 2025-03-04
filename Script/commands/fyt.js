eb948cf203a621dddb450ac85c4b943ed57ee0c4
Ullashbackup <affarhana4@gmail.com>
2025-03-04 15:42:21 +0600

    Update fyt.js

--- Script/commands/fyt.js
+++ Script/commands/fyt.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "boom",
     version: "1.0.0",
     hasPermssion: 2,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "MAHBUB SHAON",
     description: "War In Chatbox",
     commandCategory: "wargroup",
     usages: "[fyt]",
