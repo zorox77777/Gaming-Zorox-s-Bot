adbd051548045400b9309b36cbe90b2d5be9c87a
Ullashbackup <affarhana4@gmail.com>
2025-03-11 22:50:24 +0600

    Update ｢File｣__prefix.js

--- Script/commands/｢File｣__prefix.js
+++ Script/commands/｢File｣__prefix.js
@@ -1,5 +1,3 @@
-const fs = require("fs");
-
 module.exports.config = {
   name: "prefix",
   version: "1.0.0",
@@ -13,40 +11,29 @@ module.exports.config = {
 
 module.exports.handleEvent = async ({ event, api, Threads }) => {
   var { threadID, messageID, body, senderID } = event;
-
-  // যদি ক্র
-  if (this.config.credits !== "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭") { 
-    return api.sendMessage("আপনার স্ক্রিপ্টের ক্রেডিট পরিবর্তন করা হয়েছে!", threadID, messageID);
-  }
-
+  //if (senderID == global.data.botID) return;
+  if ((this.config.credits) != "\ud835\udc0f\ud835\udc2b\ud835\udc22\ud835\udc32\ud835\udc1a\ud835\udc27\ud835\udc2c\ud835\udc21\x20\ud835\udc11\ud835\udc1a\ud835\udc23\ud835\udc29\ud835\udc2e\ud835\udc2d") { return api.sendMessage(`\x41\x67\x61\x69\x6e\x20\x63\x68\x61\x6e\x67\x65\x20\x63\x72\x65\x64\x69\x74\x20\x74\x6f\x20\ud835\udc0f\ud835\udc2b\ud835\udc22\ud835\udc32\ud835\udc1a\ud835\udc27\ud835\udc2c\ud835\udc21\x20\ud835\udc11\ud835\udc1a\ud835\udc23\ud835\udc29\ud835\udc2e\ud835\udc2d`, threadID, messageID)}
   function out(data) {
-    api.sendMessage(data, threadID, messageID);
+    api.sendMessage(data, threadID, messageID)
   }
-
   var dataThread = (await Threads.getData(threadID));
   var data = dataThread.data; 
   const threadSetting = global.data.threadData.get(parseInt(threadID)) || {};
-  
   var arr = ["mpre","mprefix","prefix", "dấu lệnh", "prefix của bot là gì","daulenh", "duong", "what prefix", "freefix", "what is the prefix", "bot dead", "bots dead", "where prefix", "what is bot", "what prefix bot", "how to use bot" ,"how use bot", "where are the bots","bot not working","bot is offline","where prefix","prefx","prfix","prifx","perfix","bot not talking","where is bot"];
-
-  arr.forEach(async i => {
+  arr.forEach(i => {
     let str = i[0].toUpperCase() + i.slice(1);
-    if (body === i.toUpperCase() || body === i || str === body) {
-      const prefix = threadSetting.PREFIX || global.config.PREFIX;
-
-      // 
-      const gifURL = "https://i.imgur.com/gSW285Z.gif"; 
-
-      const msg = {
-        body: `This Is My Prefix ⇉ [ ${prefix} ]\n💝🥀𝐎𝐖𝐍𝐄𝐑:- ULLASH 💫\n🖤𝚈𝚘𝚞 𝙲𝚊𝚗 𝙲𝚊𝚕𝚕 𝙷𝚒𝚖 Ullash ッ\n😳𝐇𝐢𝐬 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐢𝐝🤓:- ☞ www.facebook.com/100000654976375\n👋For Any Kind Of Help Contact On Telegram channel Username 👉 @Online_income_bd7187`,
-        attachment: await global.utils.getStreamFromURL(gifURL) 
-      };
-
-      return api.sendMessage(msg, threadID);
+    if (body === i.toUpperCase() | body === i | str === body) {
+const prefix = threadSetting.PREFIX || global.config.PREFIX;
+      if (data.PREFIX == null) {
+        return out(`This Is My Prefix ⇉ [ ${prefix} ]\n\n𝐎𝐖𝐍𝐄𝐑:- ☞ULLASH × ☆𝐀𝐁𝐇𝐑𝐀𝐍𝐈𝐋☆\n\n𝚈𝚘𝚞 𝙲𝚊𝚗 𝙲𝚊𝚕𝚕 𝙷𝚒𝚖 ULLASH ッ\n😳𝐇𝐢𝐬 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐢𝐝:- ☞ www.facebook.com/100000654976375\n
+For Any Kind Of Help Contact On massager×✓ m.me/100000654976375 😇`)
+      }
+      else return out('️️️️️️️️️️️️️️️️️️️️️️️️️️️This Is My Prefix ⇉ [ ${prefix} ]  \n\n𝐎𝐖𝐍𝐄𝐑:- ☞ULLASH × ☆𝐀𝐁𝐇𝐑𝐀𝐍𝐈𝐋☆\n\n𝚈𝚘𝚞 𝙲𝚊𝚗 𝙲𝚊𝚕𝚕 𝙷𝚒𝚖 ULLASH ッ\n😳𝐇𝐢𝐬 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐢𝐝:- ☞ www.facebook.com/100086680386976\nFor Any Kind Of Help Contact On massager×✓ m.me/100000654976375 ' + data.PREFIX)
     }
+
   });
 };
 
 module.exports.run = async({ event, api }) => {
-  return api.sendMessage("error", event.threadID);
-};
+    return api.sendMessage("error", event.threadID)
+}
